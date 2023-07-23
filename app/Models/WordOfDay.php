b9fd105c65f6f9e4ffbe162d760665c362df71bc
filipegoncalo <filipe.gsantos13@gmail.com>
2023-07-23 01:17:57 -0300

    T-7: dispatching event when creating a new WordOfDay

--- app/Models/WordOfDay.php
+++ app/Models/WordOfDay.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
+use App\Events\WordOfDayCreatedEvent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class WordOfDay extends Model
 {
     use HasFactory;
+
     protected $guarded = [];
+
+    protected $dispatchesEvents = [
+        'created' => WordOfDayCreatedEvent::class
+    ];
 }
